f2291603cbd3d2c059720cee8eb885bbadbd33ca
Sultan Rifaldy <sultanrifaldy@gmail.com>
2023-10-22 19:58:22 +0700

    Initial Commit

---
+++ Berita Indo CNN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Berita Indo CNN
+//
+//  Created by Sultan Rifaldy on 22/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
